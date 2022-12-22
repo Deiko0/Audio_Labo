f76886efa7278d84b8c4dcb3bfe4f88699ff72ce
Deiko0 <118802900+Deiko0@users.noreply.github.com>
2022-12-22 16:18:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import japanize_matplotlib
+import base64
 from PIL import Image
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
@@ -198,6 +199,21 @@ def _set_block_container_style(max_width: int = GRAPH_WIDTH + 100, max_width_100
         }}
         </style>""", unsafe_allow_html=True)
 
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
 
 def main():
     st.title('音声配信の機材ラボ')
@@ -312,3 +328,4 @@ def main():
 if __name__ == "__main__":
     _set_block_container_style()
     main()
+    add_bg_from_local('bg.png')    
